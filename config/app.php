39e64c96ddf8bb2f607781b5af43d52e7399d30c
Zoran <zbogoevski@gmail.com>
2021-10-06 11:05:40 +0200

    Fix Pusher error

--- config/app.php
+++ config/app.php
@@ -239,7 +239,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
+        'Pusher' => Pusher\Pusher::class
 
     ],
 
